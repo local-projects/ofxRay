d75c9759ae31dca01ca02a6df595c79c36dd08d0
Oriol Ferrer Mesià <oriol@uri.cat>
2016-08-15 15:30:12 -0400

    basic addon_config.mk

---
+++ addon_config.mk
@@ -0,0 +1,9 @@
+meta:
+	ADDON_NAME = ofxRay
+	ADDON_DESCRIPTION = Lightweight Ray class and operations for openFrameworks
+	ADDON_AUTHOR = Eliott Woods
+	ADDON_TAGS = "ray" "intersection" "physics"
+	ADDON_URL = https://github.com/elliotwoods/ofxRay
+
+common:
+	ADDON_INCLUDES = src
